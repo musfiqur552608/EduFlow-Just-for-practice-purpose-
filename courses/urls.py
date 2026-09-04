487ba217989bb5196ecc7ee1ef4564209b131e60
musfiq <musfiqurrahmansayed@gmail.com>
2026-09-04 21:37:38 +0600

    add course creation, update, delete

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,7 @@
 # courses/urls.py
 
 from django.urls import path
-# from . import views
+from . import views
 from .views import CourseListView, CourseDetailView
 
 app_name = 'courses' 
@@ -11,4 +11,7 @@ urlpatterns = [
     # path('<slug:course_slug>/', views.course_detail, name='course-detail'),
     path('', CourseListView.as_view(), name='course-list'),
     path('<slug:course_slug>/', CourseDetailView.as_view(), name='course-detail'),
+    path('create/', views.course_create, name='course-create'),
+    path('<slug:course_slug>/edit/', views.course_update, name='course-update'),
+    path('<slug:course_slug>/delete/', views.course_delete, name='course-delete'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.decorators import login_required
 from .models import Course
-
+from .forms import CourseForm
 
 # def course_list(request):
 #     courses = Course.objects.filter(is_published=True)
@@ -29,4 +30,38 @@ class CourseDetailView(DetailView):
     context_object_name = 'course'
     slug_field = 'slug'
     slug_url_kwarg = 'course_slug'
+
+@login_required
+def course_create(request):
+    if request.method == 'POST':
+        form = CourseForm(request.POST)
+        if form.is_valid():
+            course = form.save(commit=False)
+            course.instructor = request.user
+            course.save()
+            return redirect('courses:course-detail', course_slug=course.slug)
+    else:
+        form = CourseForm()
+    return render(request, 'courses/course_form.html', {'form': form})
+@login_required
+def course_update(request, course_slug):
+    course = get_object_or_404(Course, slug=course_slug, instructor=request.user)
+    if request.method == 'POST':
+        form = CourseForm(request.POST, instance=course)
+        if form.is_valid():
+            form.save()
+            return redirect('courses:course-detail', course_slug=course.slug)
+    else:
+        form = CourseForm(instance=course)
+    return render(request, 'courses/course_form.html', {'form': form})
+
+
+@login_required
+def course_delete(request, course_slug):
+    course = get_object_or_404(Course, slug=course_slug, instructor=request.user)
+    if request.method == 'POST':
+        course.delete()
+        return redirect('courses:course-list')
+    return render(request, 'courses/course_confirm_delete.html', {'course': course})
+
 
